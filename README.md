4294087f4f3c26bb71496b38801ebaf7d2de7b38
Sylwester Grzywna <sgrzywna@gmail.com>
2018-07-29 13:27:08 +0200

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # statuslight
-Simple web service to set Mi-Light lamp colour depending on received status.
+
+Simple web service to set Mi-Light lamp colour depending on received status. Uses [milightd](https://github.com/sgrzywna/milightd) to control the lamp.
+
+I use it together with CI based on Jenkins to show status of tests - green means all modules passed tests, yellow means some of the modules didn't make it, and red means catastrophic failure :-)
---
+++ statuslight.go
@@ -0,0 +1,189 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+type statusType int
+
+const (
+	statusOK statusType = iota
+	statusUnstable
+	statusError
+
+	maxStatuses = 16
+)
+
+var (
+	// errTooMuchStatuses is returnes when statuses queue is full.
+	errTooMuchStatuses = errors.New("too much statuses")
+)
+
+// context represents status context, it stores all details necessary to calculate current status.
+type context struct {
+	host       string
+	port       int
+	stats      map[string]bool
+	client     *http.Client
+	colors     map[statusType]string
+	brightness int
+}
+
+// status stores status details
+type status struct {
+	State bool   `json:"state"`
+	ID    string `json:"statusId"`
+}
+
+// processStatus process status received by http server.
+func (c *context) processStatus(s status) error {
+	if len(c.stats) == maxStatuses {
+		return errTooMuchStatuses
+	}
+	c.stats[s.ID] = s.State
+	sts := c.getStatus()
+	return c.setLight(sts)
+}
+
+// getStatus returns single status for all received statuses.
+func (c *context) getStatus() statusType {
+	var t, f int
+	for _, s := range c.stats {
+		switch s {
+		case true:
+			t++
+		case false:
+			f++
+		}
+	}
+	res := statusOK
+	if t > 0 {
+		if f > 0 {
+			res = statusUnstable
+		}
+	} else {
+		if f > 0 {
+			res = statusError
+		}
+	}
+	return res
+}
+
+// setLight sets light through milightd.
+func (c *context) setLight(s statusType) error {
+	url := fmt.Sprintf("http://%s:%d/api/v1/light", c.host, c.port)
+
+	color, ok := c.colors[s]
+	if !ok {
+		return fmt.Errorf("milightd client: unsupported status: %d", s)
+	}
+
+	var cmd = struct {
+		Color      string `json:"color"`
+		Brightness int    `json:"brightness"`
+		Switch     string `json:"switch"`
+	}{
+		Color:      color,
+		Brightness: c.brightness,
+		Switch:     "on",
+	}
+
+	d, err := json.Marshal(cmd)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("PUT", url, bytes.NewBuffer(d))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("milightd client: unexpected status code: %d", resp.StatusCode)
+	}
+
+	return nil
+}
+
+func main() {
+	var mihost = flag.String("mihost", "127.0.0.1", "milightd network address")
+	var miport = flag.Int("miport", 8080, "milightd network port")
+	var port = flag.Int("port", 8888, "listening port")
+	var okColor = flag.String("ok-color", "green", "color for the OK status")
+	var unstableColor = flag.String("unstable-color", "yellow", "color for the unstable status")
+	var errorColor = flag.String("error-color", "red", "color for the error status")
+	var brightness = flag.Int("brightness", 32, "brightness level")
+
+	flag.Parse()
+
+	ctx := context{
+		host:  *mihost,
+		port:  *miport,
+		stats: make(map[string]bool),
+		client: &http.Client{
+			Timeout: time.Second * 10,
+		},
+		colors: map[statusType]string{
+			statusOK:       *okColor,
+			statusUnstable: *unstableColor,
+			statusError:    *errorColor,
+		},
+		brightness: *brightness,
+	}
+
+	r := mux.NewRouter()
+	v1 := r.PathPrefix("/api/v1/").Subrouter()
+
+	v1.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+		statusHandler(w, r, &ctx)
+	}).Methods("POST")
+
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         fmt.Sprintf(":%d", *port),
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Printf("statuslight listening @ :%d\n", *port)
+	log.Fatal(srv.ListenAndServe())
+}
+
+func statusHandler(w http.ResponseWriter, r *http.Request, ctx *context) {
+	var s status
+	if r.Body == nil {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&s)
+	if err != nil {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	err = ctx.processStatus(s)
+	if err != nil {
+		log.Printf("processStatus error: %s\n", err)
+		http.Error(w, "statuslight error", http.StatusInternalServerError)
+		return
+	}
+}
